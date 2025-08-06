85b8251c22491e809215a236963b34d804791ca6
Igor P. <igorpetrunyok@gmail.com>
2025-08-06 18:49:11 +0300

    Оновлено секцію

--- js/task-2.js
+++ js/task-2.js
@@ -2,3 +2,6 @@ function getShippingMessage(country, price, deliveryFee) {
   const totalPrice = price + deliveryFee;
   return `Shipping to ${country} will cost ${totalPrice} credits`;
 }
+console.log(getShippingMessage('Germany', 120, 50)); // Shipping to Germany will cost 170 credits
+console.log(getShippingMessage('USA', 80, 20)); // Shipping to USA will cost 100 credits
+console.log(getShippingMessage('Canada', 100, 20)); // Shipping to Canada will cost 120 credits
